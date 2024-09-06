3dce1da3640dbb459484885447b2cdc0dee66e56
phandinhtrong <phandinhtrong@.gmail>
2024-09-06 14:40:01 +0700

    hahhaaa

---
+++ BaiTap05/BaiTap05/Controllers/TheLoaiController.cs
@@ -0,0 +1,26 @@
+﻿using BaiTap05.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap05.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Ngay = "ngày 28";
+            ViewBag.Thang = "tháng 100";
+            ViewData["nam"] = "nam 2024";
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            var theloai = new TheLoaiViewModel
+            {
+                Id = 1,
+                Name = "Trinh thám"
+            };
+            return View(theloai);
+        }
+    }
+}
