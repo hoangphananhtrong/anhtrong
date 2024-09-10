382c16849b569f0cb7a17152462a93b87c5768a6
phandinhtrong <phandinhtrong@.gmail>
2024-09-10 15:28:00 +0700

    btktra

---
+++ BaiTapKiemTra01/BaiTapKiemTra01/Models/TaiKhoanViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapKiemTra01.Models
+{
+    public class TaiKhoanViewModel : Controller
+    {
+        public int Id { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+    }
+}
