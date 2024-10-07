0e419049fe9cf1e158ff668c4ef6586ecd68a1f2
phandinhtrong <phandinhtrong@.gmail>
2024-10-07 16:40:40 +0700

    hahah

---
+++ BaiTap07a/BaiTap07a/Data/Migrations/20241001074120_YourMigrationName.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BaiTap07a.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class YourMigrationName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "TheLoai",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "TheLoai",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldMaxLength: 10);
+        }
+    }
+}
---
+++ BaiTap07a/BaiTap07a/Models/TheLoai.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaiTap07a.Models
+{
+    public class TheLoai
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required (ErrorMessage ="không thể để trống tên thể loại!")]
+        [StringLength (10, ErrorMessage = "{0} độ dài từ  {2} đến {1} ký tự.",  MinimumLength = 8)]
+        [Display(Name ="Thể loại")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "không đúng định dạng ngày!")]
+        [Display(Name = "Ngày tạo")]
+        public DateTime DataCreated { get; set; } = DateTime.Now;
+    }
+}
